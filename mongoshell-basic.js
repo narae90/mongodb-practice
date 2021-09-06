5638e4d0414e7be24aa572ff949a6824f5a5269d
김나래 <narae900@naver.com>
2021-09-06 15:38:51 +0900

    Projection, Sort

--- mongoshell-basic.js
+++ mongoshell-basic.js
@@ -138,6 +138,97 @@ db.posts.find()
 // .pretty() 메소드 : BSON을 보기 좋게 출력
 
 
+// 검색 조건 연산자
+// 같다 : {key: value}
+db.posts.find({"by": "bit"}) 
+
+
+// 크다 : $gt {key: {$gt:value}}
+db.posts.find({"likes":{$gt:30}})
+// 크거나 같다 : $gte
+// 작다 : $lt
+// 작거나 같다: $lte
+// 같지 않다 : $ne
+
+// 조건의 조합
+// and연산 
+/*
+ {
+    $and: [
+	{조건 객체 1},
+	{조건 객체 2}
+       ]
+}
+
+*/
+
+// by가 hong이고 likes <= 30
+db.posts.find(
+	{
+		$and: [
+			{by: "hong"},
+			{likes: {$lte: 30}
+		]
+	
+})
+
+
+// or 연산
+/*
+  {
+    $or: [
+      { 조건 객체 1 },
+      { 조건 객체 2 }
+    ]
+  }
+*/
+// by가 hong이고 likes <= 30
+db.posts.find({
+		$and: [
+			{ by: "hong" },
+			{ likes: { $lte: 30 } }
+		]
+	}
+)
+
+// Projection
+// find, findOne의 두번째 인자 객체로 출력 필드를 명시
+//  출력을 제어할 수 있다.
+//  1: 출력함, 0: 출력하지 않음
+// 모든 문서들 중에서, _id 출력하지 않고
+//  title, likes 정보 출력
+db.posts.find({}, { title: 1, likes: 1, _id: 0 })
+
+// 출력 제한
+//  .limit : 받아올 갯수를 제한
+//  .skip : 건너뛸 문서의 갯수 지정
+db.posts.find().limit(2).skip(2)
+
+// 데이터 정렬
+//  .sort : 정렬 기준 객체
+//	1: 오름차순, -1: 내림차순
+
+// likes의 역순으로 정렬
+db.posts.find().sort({likes: -1})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
