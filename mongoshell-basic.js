6d6d0ecada1ffcb77da69402b1a7a16b040623ae
김나래 <narae900@naver.com>
2021-09-06 14:48:28 +0900

    insert, find, findOne

--- mongoshell-basic.js
+++ mongoshell-basic.js
@@ -57,3 +57,87 @@ db.posts.save(post)
 
 
 
+// 기존 문서의 갱신 (Update)
+/* db.컬렉션명.update(
+	{변경 문서의 조건},
+	{$set :
+		{업데이트 할 내용}
+	}
+);
+*/
+
+
+db.posts.update(
+	{"title": :First Post"},
+	{$set:
+		{createdAt: new Date(),
+		updatedAt: new Date()}
+	}
+)
+
+
+// 객체의 삭제 : .remove
+post = db.posts.findOne()
+db.posts.remove(post)
+
+// 검색 조건 객체를 이용한 삭제
+db.posts.remove({title: /Second/})
+
+
+/*
+db.posts 컬렉션에
+title: "First Post", by: "bit", likes: 10
+title: "Second Post", by: "hong", likes: 50
+title: "Third Post", by: "bit", likes: 30
+title: "Fourth Post", by: "hong", likes: 10
+
+INSERT 연습
+*/
+
+db.posts.insert({
+	title: "First Post",
+	by: "bit",
+	likes: 10
+})
+
+db.posts.insert({
+	title: "Second Post",
+	by: "hong", 
+	likes: 50
+})
+
+db.posts.insert({
+	title: "Third Post",
+	by: "bit",
+	likes: 30
+})
+
+db.posts.insert({
+	title: "Fourth Post",
+	by: "hong", 
+	likes: 10
+})
+
+
+// 여러 문서를 insert
+db.posts.insertMany([
+	{title: "Fifth Post",
+		by: "bit",
+		likes: 50},
+	{title: "Sixth Post",
+		by: "hong", 
+		likes: 50}
+])
+
+
+// 문서의 검색
+// findOne: 조건을 만족하는 문서 중 1개를 반환
+// find(): 조건을 만족하는 문서의 커서를 반환
+db.posts.findOne()
+db.posts.find()
+// .pretty() 메소드 : BSON을 보기 좋게 출력
+
+
+
+
+
