5139a526c30ce209f90856a25d0a7720b5ae1d7c
김나래 <narae900@naver.com>
2021-09-09 11:37:01 +0900

    API Server 구현

--- app.js
+++ app.js
@@ -1,126 +1,125 @@
-//* Express 모듈 불러오기
+//  Express 모듈 불러오기
 const express = require("express");
-const http = require("http"); // 노드 기본 http 모듈
+const http = require("http");   //  노드 기본 http 모듈
 
-
-//* express 생성
+//  express 생성
 const app = express();
 
-// 설정 , 속성을 집어 넣을때 
-// set(키, 값)
-// get(키)
+//  설정, 속성을 집어넣을 때
+//  set(키, 값)
+//  get(키)
 
-// etag를 사용하지 않음
+//  etag를 사용하지 않음  
 app.set("etag", false);
 
-
-//* 환경 변수에  PORT 가 설정되어 있으면 그 값을 사용
-//* 설정이 안되어 있으면 3000
+//  환경 변수에 PORT 가 설정되어 있으면 그 값을 사용
+//  설정이 안되어 있으면 3000
 app.set('port', process.env.PORT || 3000);
 
-// 로거 추가
-// npm install morgan
-// 로커 불러오기
-const logger = require("morgan"); // 로거 불러오기
-// 로거를 express에 추가 : 미들웨어 추가
+//  로거 추가
+//  npm install morgan
+//  로거 불러오기
+const logger = require("morgan");   //  로거 불러오기
+//  로거를 express에 추가: 마들웨어 추가
 app.use(logger("dev"));
 
-// MongoDB
+//  MongoDB
 const { MongoClient } = require("mongodb");
+const APIRouter = require("./router/APIRouter");
 
-
-
-// 정적 웹의 제공
-// 미들웨어  express.static  미들웨어 함수를 등록
+//  정적 웹의 제공
+//  미들웨어 express.static 미들웨어 함수를 등록 
 app.use(express.static(__dirname + "/public"));
 
-//* view 엔진 설정
-app.set("view engine", "ejs");  //* 뷰엔진으로 ejs 사용선언
-app.set("views", __dirname + "/views"); //* 템플릿의 위치
+//  body-parser 등록
+//  4.16 버전 이후에는 express 내부에 bodyParser가 포함 
+//  POST 요청을 처리할 수 있게 된다.
+app.use(express.urlencoded({ extended: false }));
 
+//  View 엔진 설정
+app.set("view engine", "ejs");  //  뷰엔진으로 ejs 사용 선언
+app.set("views", __dirname + "/views"); //  템플릿의 위치
 
-//* GET 메소드 요청의 처리
-//*  app.get(url, callback)
+//  GET 메서드 요청의 처리 
+//  app.get(url, callback)
 app.get("/", (req, resp) => {
-    //* http 모듈의 응답 처리 메소드
+    //  http 모듈의 응답 처리메서드
     /*
-    console.log("[GET] /" );
-    resp.writeHead(200, {'Content-Type': 'text/html; charset=UTF-8'});
+    console.log("[GET] /");
+    resp.writeHead(200, {'Content-Type': 'text/html;charset=UTF-8'});
     resp.write("Express Welcomes You!");
     resp.end();
     */
     resp.status(200)
-        .contentType("text/html;charset=UTF-8")
+        .contentType("text/html;charset=utf-8")
         .render("home");
 });
 
-app.get("/Welcome", (req, resp) => {
-    //* express의 추가 응답 처리 메소드
+app.get("/welcome", (req, resp) => {
+    //  express의 추가 응답 처리 메서드
     console.log("[GET] /welcome");
     resp.status(200)
-        .header("Content-Type", "text/html;charset=UTF-8")
+        .header("Content-Type", "text/html;charset=utf-8")
         .send("Welcome!");
-
 })
 
-
-//* GET 요청 파라미터의 처리
-app.get("/request",(req, resp) => {
+//  GET 요청 파라미터의 처리
+app.get("/request", (req, resp) => {
     console.log("[GET] /request");
-    console.log(req.query); //* req.query -> url 파라미터 객체
+    console.log(req.query); //  req.query -> url 파라미터 객체
     console.log("[QUERY] name:" + req.query.name);
 
     let paramName = req.query.name;
-    if(paramName === undefined || paramName.length == 0){   //* name 파라미터가 전달되지 않으면
-        resp.status(404)    // Not Found
-            .contentType("text/html;charset=UTF-8")
+    if (paramName === undefined ||
+        paramName.length == 0) {    //  name 파라미터가 전달되지 않으면
+        resp.status(404)    //  Not Found
+            .contentType("text/html;charset=utf-8")
             .send("Name 정보를 확인할 수 없어요.");
-    } else{
-        // 파라미터 정상 전달
-        resp.status(200)    // 성공
-            .contentType("text/html;charset=UTF-8")
+    } else {
+        //  파라미터 정상 전달
+        resp.status(200)    //  성공
+            .contentType("text/html;charset=utf-8")
             .send("Name:" + paramName);
     }
-
 })
 
-
-//* URL 파라미터 처리(Fancy URL, Pretty URL)
-//* URL 의 경로일부로 데이터 전송 방식
+//  URL 파라미터 처리(Fancy URL, Pretty URL)
+//  URL의 경로일부로 데이터 전송 방식
 app.get("/urlparam/:name", (req, resp) => {
-    //* url 파라미터는 params 객체로 얻어온다.
+    //  url 파라미터는 params 객체로 얻어온다.
     let userName = req.params.name;
-    resp.writeHead(200, {"Content-Type": "text/html;charset=UTF-8"});
+    resp.writeHead(200, { "Content-Type": "text/html;charset=UTF-8"});
     resp.write("<h1>name:" + userName + "</h1>")
     resp.write("<p>URL 파라미터를 전달 받았습니다.</p>")
     resp.send();
 });
 
-//* 뷰엔진 활용
+//  뷰엔진 활용
 app.get("/render", (req, resp) => {
-    //* 응답객체의 render 메소드 활용
-    resp.contentType("text/html;charset=UTF-8")
+    //  응답객체의 render 메서드 활용
+    resp.contentType("text/html;charset=utf-8")
         .render("render");
-})
+});
 
-
-//* Router 등록 (미들웨어)
+//  Router 등록(미들웨어)
 const webRouter = require("./router/WebRouter")(app);
 app.use("/web", webRouter);
+const apiRouter = require("./router/APIRouter")(app);
+app.use("/api", apiRouter);
 
 function startServer() {
-    //*  database 연결 정보
+    //  database 연결 정보
     const dburl = "mongodb://localhost:27017";
-    //*  데이터베이스 Connect
+    //  데이터베이스 Connect
     MongoClient.connect(dburl, { useNewUrlParser: true })
         .then(client => {
-            //*  db 선택
+            //  db 선택
             console.log("데이터베이스에 연결 되었습니다.");
             let db = client.db("mydb")
-            //*  익스프레스에 추가
+            //  익스프레스에 추가
             app.set("db", db); //   db 키로 몽고 클라이언트 추가
 
-            //*  express 실행
+            //  express 실행
             startExpress();
         })
         .catch(reason => {
@@ -128,13 +127,11 @@ function startServer() {
         });
 }
 
-function startExpress(){
-
-    //* 서버 start
-    http.createServer(app).listen(app.get("port"), ()=> {
+function startExpress() {
+    //  서버 start
+    http.createServer(app).listen(app.get("port"), () => {
         console.log("Web Server is running on port:" + app.get("port"));
     })
-
 }
 
 startServer();
--- router/WebRouter.js
+++ router/WebRouter.js
@@ -1,14 +1,82 @@
 const express = require("express");
 const router = express.Router();
+const { ObjectId } = require("mongodb");    //  ObjectId를 처리하기 위한 객체
 
-module.exports =(app) => {
+module.exports = (app) => {
     router.get(["/friends/list", "/friends"], (req, resp) => {
-        resp.status(200)
-        .contentType("text/html;charset=UTF-8")
-        .send("<p>Web Router 응답 </p>");
-
-        
+        //  express에서 db 객체 꺼내고
+        let db = app.get("db");
+        //  쿼리 수행
+        db.collection('friends').find().toArray()
+        .then(result => {
+            console.log(result);
+            //  결과를 템플릿에 반영
+            resp.render("friends_list", { friends: result });
+        })
+        .catch(reason => {
+            console.lerror(reason);
+        })
+        // resp.status(200)
+        //     .contentType("text/html;charset=utf-8")
+        //     .send("<p>Web Router 응답</p>");
     });
 
+    //  새 친구 등록 폼
+    router.get("/friends/new", (req, resp) => {
+        resp.render("friend_insert_form");
+    })
+
+    //  새 친구 등록 액션
+    router.post("/friends/save", (req, resp) => {
+        //  폼 정보는 req.body
+        console.log(req.body);
+        let document = req.body;
+        document.age = parseInt(document.age);
+
+        let db = app.get("db");
+
+        db.collection("friends").insertOne(document)
+        .then(result => {
+            console.log(result);
+            resp.status(200)
+                .send("SUCCESS: 친구를 추가했습니다.");
+        })
+        .catch(reason => {
+            console.error(reason);
+            resp.status(500)    //  Internal Server Error
+                .send("ERROR: 친구를 추가하지 못했습니다.");
+        })
+    })
+    //  사용자 정보 확인
+    router.get("/friends/show/:id", (req, resp) => {
+        console.log("id:", req.params.id);
+
+        let db = app.get("db");
+        db.collection('friends')
+        .findOne({_id: ObjectId(req.params.id)})
+        .then(result => {
+            resp.render("friend_show", { friend: result });
+        })
+        .catch(reason => {
+            resp.status(500)
+                .send("<p>사용자 정보가 없습니다</p>");
+        })
+    });
+
+    //  삭제
+    router.get("/friends/delete/:id", (req, resp) => {
+        console.log("삭제할 ID:", req.params.id);
+        let db = app.get("db");
+        db.collection("friends")
+        .deleteOne({_id: ObjectId(req.params.id)})
+        .then(result => {
+            //  리스트 페이지로
+            resp.redirect("/web/friends/list");
+        })
+        .catch(reason => {
+            resp.status(500)
+                .send("<p>삭제할 수 없습니다.</p>");
+        })
+    })
     return router;
 }
